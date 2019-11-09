a7ccd1f4e9c530920ec6c26fbc4f3b97f10c2be6
huma <yuto101007@gmail.com>
2019-11-09 19:15:58 +0900

    create product page & build

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,12 +3,13 @@
         <div class= "list-container">
             <div
             class="list-item"
-            v-for="item in listItems"
-            v-bind:key="item.id">{{item.title}}</div>
+            v-for="(item,index) in listItems"
+            v-bind:key="item.id"
+            v-on:click="changeProduct(index)">{{item.title}}</div>
         </div>
         <div class="explanation">
-            <h3>title</h3>
-            <img alt="vue logo" src="../assets/logo.png">
+            <h3 >{{nowTitle}}</h3>
+            <img alt="vue logo" v-bind:src="nowImageSource">
             <p>explanation</p>
         </div>
     </div>
@@ -20,10 +21,24 @@ export default {
   data: function () {
     return {
       listItems: [
-        {title: 'DragonSlayer'},
-        {title: 'FortuneTelling'},
-        {title: 'TETROCK'}
-      ]
+        {title: 'DragonSlayer', source: require('@/assets/logo.png')},
+        {title: 'FortuneTelling', source: require('@/assets/logo.png')},
+        {title: 'TETROCK', source: require('@/assets/logo.png')}
+      ],
+      selecting: 0
+    }
+  },
+  methods: {
+    changeProduct: function (title) {
+      this.selecting = title
+    }
+  },
+  computed: {
+    nowTitle: function () {
+      return this.listItems[this.selecting].title
+    },
+    nowImageSource: function () {
+      return this.listItems[this.selecting].source
     }
   }
 }
@@ -43,6 +58,7 @@ export default {
 .list-item{
     height: 50px;
     line-height: 50px;
+    cursor: pointer;
 }
 
 .explanation {
